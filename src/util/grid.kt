b3d58d677f4b89cd0b33973069b975b324c72abc
Brian Parma <execrable@gmail.com>
2024-12-06 10:31:32 -0700

    add more grid functions

--- src/util/grid.kt
+++ src/util/grid.kt
@@ -4,7 +4,15 @@ interface Grid<T> {
     val xRange: LongRange
     val yRange: LongRange
 
+    val indices get() = yRange.asSequence().flatMap { y -> xRange.asSequence().map { x -> LongPoint(x, y) } }
+
     operator fun get(point: LongPoint): T?
+
+    operator fun contains(point: LongPoint): Boolean
+
+    fun copy(new: Map<LongPoint, T>): Grid<T>
+
+    fun find(block: (LongPoint, T) -> Boolean): LongPoint?
 
     fun findAll(block: (LongPoint, T) -> Boolean): List<LongPoint>
 }
@@ -12,6 +20,16 @@ interface Grid<T> {
 data class MapGrid<T>(val data: Map<LongPoint, T>, override val xRange: LongRange, override val yRange: LongRange) :
     Grid<T>, Map<LongPoint, T> by data {
     override operator fun get(point: LongPoint): T? = data[point]
+
+    override fun contains(point: LongPoint): Boolean = point in data
+
+    override fun copy(new: Map<LongPoint, T>): MapGrid<T> {
+        return this.copy(data = data + new)
+    }
+
+    override fun find(block: (LongPoint, T) -> Boolean): LongPoint? =
+        entries.firstOrNull { (k, v) -> block(k, v) }?.key
+
 
     override fun findAll(block: (LongPoint, T) -> Boolean) = entries.filter { (k, v) -> block(k, v) }.map { it.key }
 }
@@ -24,6 +42,28 @@ value class StringGrid(val data: List<String>) :
 
     override fun get(point: LongPoint): Char? {
         return data.getOrNull(point.y.toInt())?.getOrNull(point.x.toInt())
+    }
+
+    override fun contains(point: LongPoint): Boolean = point.x in xRange && point.y in yRange
+
+    override fun copy(new: Map<LongPoint, Char>): Grid<Char> = buildList {
+        yRange.forEach { y ->
+            add(buildString {
+                xRange.forEach { x ->
+                    append(new[x point y] ?: get(x point y))
+                }
+            })
+        }
+    }.toGrid()
+
+    override fun find(block: (LongPoint, Char) -> Boolean): LongPoint? {
+        data.forEachIndexed { y, row ->
+            row.forEachIndexed { x, char ->
+                val p = x.toLong() point y.toLong()
+                if (block(p, char)) return p
+            }
+        }
+        return null
     }
 
     override fun findAll(block: (LongPoint, Char) -> Boolean): List<LongPoint> = buildList {
@@ -41,7 +81,7 @@ fun String.toGrid(delimiter: String = "\n") = split(delimiter).toGrid()
 
 fun List<String>.toGrid(): Grid<Char> = StringGrid(this)
 
-fun List<String>.toMapGrid(): Grid<Char> = MapGrid(
+fun List<String>.toMapGrid(): MapGrid<Char> = MapGrid(
     data = buildMap {
         forEachIndexed { r, row ->
             row.forEachIndexed { c, char ->
