b78de08f56ab5c0214aa39b7c2e708e4bb6e4b9d
Brian Parma <execrable@gmail.com>
2025-12-03 22:55:37 -0700

    more d3 cleanup

--- src/year2025/day03.kt
+++ src/year2025/day03.kt
@@ -24,16 +24,16 @@ object Day3 : Solutions {
     val first = puzzle {
         part1 {
             lines.sumOf { line ->
-                val max = line.dropLast(1).withIndex().maxBy { it.value }
-                (max.value.toString() + line.drop(max.index + 1).max()).toInt()
+                val (idx, max) = line.dropLast(1).withIndex().maxBy { it.value }
+                "$max${line.drop(idx + 1).max()}".toInt()
             }
         }
 
         part2 {
             fun findMax(bank: String, n: Int): String {
                 if (n == 0) return ""
-                val max = bank.dropLast(n - 1).withIndex().maxBy { it.value }
-                return max.value + findMax(bank.drop(max.index + 1), n - 1)
+                val (index, max) = bank.dropLast(n - 1).withIndex().maxBy { it.value }
+                return max + findMax(bank.drop(index + 1), n - 1)
             }
             lines.sumOf { line ->
                 findMax(line, 12).toLong()
@@ -45,22 +45,21 @@ object Day3 : Solutions {
         part2 {
             tailrec fun String.findMax(n: Int, jolts: Long = 0): Long {
                 if (n == 0) return jolts
-                val max = dropLast(n - 1).withIndex().maxBy { it.value }
-                return drop(max.index + 1).findMax(n - 1, 10 * jolts + (max.value - '0'))
+                val (index, max) = dropLast(n - 1).withIndex().maxBy { it.value }
+                return drop(index + 1).findMax(n - 1, 10 * jolts + max.digitToInt())
             }
             lines.sumOf { line ->
                 line.findMax(12)
             }
         }
-
     }
 
     val seq = puzzle {
         part2 {
             lines.sumOf { line ->
                 (12 downTo 1).fold("" to line) { (m, left), i ->
-                    val max = left.dropLast(i - 1).withIndex().maxBy { it.value }
-                    (m + max.value) to left.drop(max.index + 1)
+                    val (index, max) = left.dropLast(i - 1).withIndex().maxBy { it.value }
+                    (m + max) to left.drop(index + 1)
                 }.first.toLong()
             }
         }
